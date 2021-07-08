eb1e10868dff554c3a4b38217264d89213f10a60
Mishanya644 <ivanov0604041@gmail.com>
2021-07-08 18:07:33 +0300

    bot is up

--- userInterface-telegram/bot.js
+++ userInterface-telegram/bot.js
@@ -1,1 +1,19 @@
 const {Telegraf} = require('telegraf');
+const fs = require ('fs');
+
+function syncFileToStr(fileName){
+  let token = fs.readFileSync(fileName, {encoding:'utf8', flag:'r'});
+  return token;
+}
+
+const bot = new Telegraf(syncFileToStr('./tokens/test-token.txt'));
+
+bot.start(ctx=>{
+  ctx.reply('Oh hello there!');
+})
+
+bot.command('bet', ctx=>{
+  ctx.reply('you bet!');
+})
+
+bot.launch();
