c7af83e2dd04dff5c27c5c67e3f1a2e795bdab08
Mishanya644 <ivanov0604041@gmail.com>
2021-07-12 04:08:36 +0300

    added some json functionality to database module

--- database/database.cpp
+++ database/database.cpp
@@ -4,9 +4,11 @@
 #include <chrono>
 #include <limits.h>
 #include <unistd.h>
+#include <nlohmann/json.hpp>
 
-std::string getexepath()
-{
+using json = nlohmann::json;
+
+std::string getexepath(){
   char result[ PATH_MAX ];
   ssize_t count = readlink( "/proc/self/exe", result, PATH_MAX );
   std::string fullpath = std::string( result, (count > 0) ? count : 0 );
@@ -20,21 +22,15 @@ std::string getexepath()
 
 std::string currentRoundPath = getexepath() + "current-round.txt";
 
-struct Entry{
-  std::string user = "";
-  int money = 0;
-  int time = 0;
-  Entry(std::string u, int m, int t){user = u; money = m; time = t;}
-};
-
-void currentRoundAppend(Entry entry){
+void currentRoundAddEntry(int userId, int money){
+  auto time = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
   std::fstream currentRoundFile;
   currentRoundFile.open(currentRoundPath, std::ios::app);
   if (!currentRoundFile){
     std::cout << "error opening current round file for appending!";
     throw;
   }
-  currentRoundFile << entry.user <<' '<< entry.money <<' '<< entry.time << '\n';
+  currentRoundFile << userId <<' '<< money <<' '<< time << '\n';
   currentRoundFile.close();
 }
 
@@ -48,7 +44,18 @@ void currentRoundClear(){
   currentRoundFile.close();
 }
 
-std::string getRawCurrentRound(){
+void currentRoundWriteStr(std::string str){
+  std::ofstream currentRoundFile;
+  currentRoundFile.open(currentRoundPath);
+  if(!currentRoundFile){
+    std::cout << "can't open current round file for writing!";
+    throw;
+  }
+  currentRoundFile << str;
+  currentRoundFile.close();
+}
+
+std::string getStrCurrentRound(){
   std::ifstream currentRoundFile;
   currentRoundFile.open(currentRoundPath);
   std::string line = "", str = "";
@@ -60,14 +67,20 @@ std::string getRawCurrentRound(){
 }
 
 int main(int argc, char **argv){
-  if ((std::string)argv[1] == "add_payment"){
-    auto time = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    currentRoundAppend(Entry(argv[2], std::stoi(argv[3]), time));
-  } else if ((std::string)argv[1] == "clear_current_round"){
-    currentRoundClear();
-  } else if ((std::string)argv[1] == "dump_current_round"){
-    std::cout << getRawCurrentRound();
-  } else 
-    std::cout << "command not specified!";
+  json sample;
+  sample["hey"] = 10;
+  currentRoundWriteStr(sample.dump());
+  json fileJson = json::parse(getStrCurrentRound());
+  std::cout << fileJson["hey"];
+  if(argc > 1){
+    if ((std::string)argv[1] == "add_payment"){
+      currentRoundAddEntry(std::stoi(argv[2]), std::stoi(argv[3]));
+    } else if ((std::string)argv[1] == "clear_current_round"){
+      currentRoundClear();
+    } else if ((std::string)argv[1] == "dump_current_round"){
+      std::cout << getStrCurrentRound();
+    } else 
+      std::cout << "command not specified!";
+  }
   return 0;
 }
--- database/devloop.sh
+++ database/devloop.sh
@@ -1,12 +1,7 @@
 while(true) do
   clear
   g++ database.cpp -o database
-  echo 'from dir'
-  ./database dump_current_round
-  cd ..
-  echo 'from parent'
-  ./database/database dump_current_round
-  cd database
+  ./database
   echo 'current round file contents:'
   cat current-round.txt
   read var
